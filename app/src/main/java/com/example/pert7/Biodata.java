a7a84cee5bd362fe89607c95d898394c9ed89188
Eka Fitri Ramadani <eka.efira@gmail.com>
2021-04-06 23:47:55 +0800

    git commit

--- app/src/main/java/com/example/pert7/Biodata.java
+++ app/src/main/java/com/example/pert7/Biodata.java
@@ -2,7 +2,9 @@ package com.example.pert7;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Biodata extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class Biodata extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_biodata);
+    }
+
+    public void ClickLogout(View view) {
+        MainActivity.logout(this);
+    }
+
+    public void BacktoHome(View view) {
+        Intent intent = new Intent (Biodata.this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/pert7/MainActivity.java
+++ app/src/main/java/com/example/pert7/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.pert7;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,4 +24,37 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void ClickLogout(View view) {
+        logout(this);
+    }
+
+    public static void logout(Activity activity) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        builder.setTitle("Keluar");
+        builder.setMessage("Seriously:?");
+        builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                activity.finishAffinity();
+                System.exit(0);
+            }
+        });
+        builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+    }
+
+    public void Message(View view) {
+        Intent intent = new Intent (Intent.ACTION_VIEW, Uri.parse("sms:" + "085242910658")).putExtra("sms_body", "Hai, Nama saya Eka");
+        startActivity(intent);
+    }
+
+    public void Maps(View view) {
+        Intent intent = new Intent (Intent.ACTION_VIEW, Uri.parse("geo:-5.202241,119.458722")).setPackage("com.google.android.apps.maps");
+        startActivity(intent);
+    }
 }
